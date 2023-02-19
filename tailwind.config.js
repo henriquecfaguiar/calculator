d612eb80830268eb5908c32dd56b1667210067e4
henriquecfaguiar <henriquecfaguiar@gmail.com>
2023-02-19 09:36:06 -0300

    Minor styling adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {
+      colors: {
+        'footer': '#454158',
+      },
       fontFamily: {
         'sans': ['JetBrains Mono', 'sans-serif']
       }
