3a27e7a6f02d67556cf6f80ab51b51efa0741030
henriquecfaguiar <henriquecfaguiar@gmail.com>
2023-02-19 16:53:46 -0300

    Footer minor changes and update screenshot

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {
-      colors: {
-        'footer': '#454158',
-      },
       fontFamily: {
         'sans': ['JetBrains Mono', 'sans-serif']
       }
